819f85c5b558a94a6324a2b738b9711777a8df65
yhc-yhc <yhc-yhc@users.noreply.github.com>
2016-11-15 21:20:24 +0800

    Update parent.js

--- parent.js
+++ parent.js
@@ -1,161 +1,113 @@
-var cm = require('./child2parent.js')
-var child = cm.child;
-var ev = cm.ev;
-function async_like(arg, cb){
+
+var p2c = require('./parent2child.js')
+var evn = require('./event.map.js')
+
+function async_like_add(arg, cb){
 	setTimeout(function(){
 		//console.log(arg, ev)
-		var flag = 'cal'+ arg + Date.now();
+		var flag = evn.add;
+		//console.log(flag)
+		var opt = {flag: flag, data: arg}
 
-		child.send({flag: flag, data: arg})
-		ev.once(flag, function(data){
-			cb(data)
+		p2c(opt, function(err, data){
+			cb(err, data)
 		})
 	}, 1000)
 }
 
-async_like(10, function(data){
-	console.log(10, data)
+function async_like_multi(arg, cb){
+	setTimeout(function(){
+		//console.log(arg, ev)
+		var flag = evn.multi;
+
+		var opt = {flag: flag, data: arg}
+
+		p2c(opt, function(err, data){
+			cb(err, data)
+		})
+	}, 1000)
+}
+
+async_like_add(10, function(err, data){
+	console.log(10, err, data)
 })
 
-async_like(1, function(data){
-	console.log(1, data)
+async_like_add(1, function(err, data){
+	console.log(1, err, data)
 })
 
-async_like(2, function(data){
-	console.log(2, data)
+async_like_add(2, function(err, data){
+	console.log(2, err, data)
 })
 
-async_like(3, function(data){
-	console.log(3, data)
+async_like_add(3, function(err, data){
+	console.log(3, err, data)
 })
 
-async_like(1, function(data){
-	console.log(1, data)
+async_like_add(5, function(err, data){
+	console.log(5, err, data)
 })
 
-async_like(3, function(data){
-	console.log(3, data)
+async_like_add(4, function(err, data){
+	console.log(4, err, data)
 })
 
-async_like(10, function(data){
-	console.log(10, data)
+async_like_add(6, function(err, data){
+	console.log(6, err, data)
 })
 
-async_like(1, function(data){
-	console.log(1, data)
+async_like_add(7, function(err, data){
+	console.log(7, err, data)
 })
 
-async_like(2, function(data){
-	console.log(2, data)
+async_like_add(9, function(err, data){
+	console.log(9, err, data)
 })
 
-async_like(3, function(data){
-	console.log(3, data)
+async_like_add(8, function(err, data){
+	console.log(8, err, data)
 })
 
-async_like(1, function(data){
-	console.log(1, data)
+async_like_multi(10, function(err, data){
+	console.log(10, err, data)
 })
 
-async_like(3, function(data){
-	console.log(3, data)
+async_like_multi(1, function(err, data){
+	console.log(1, err, data)
 })
 
-async_like(10, function(data){
-	console.log(10, data)
+async_like_multi(2, function(err, data){
+	console.log(2, err, data)
 })
 
-async_like(1, function(data){
-	console.log(1, data)
+async_like_multi(3, function(err, data){
+	console.log(3, err, data)
 })
 
-async_like(2, function(data){
-	console.log(2, data)
+async_like_multi(5, function(err, data){
+	console.log(5, err, data)
 })
 
-async_like(3, function(data){
-	console.log(3, data)
+async_like_multi(4, function(err, data){
+	console.log(4, err, data)
 })
 
-async_like(1, function(data){
-	console.log(1, data)
+async_like_multi(6, function(err, data){
+	console.log(6, err, data)
 })
 
-async_like(3, function(data){
-	console.log(3, data)
+async_like_multi(7, function(err, data){
+	console.log(7, err, data)
 })
 
-async_like(10, function(data){
-	console.log(10, data)
+async_like_multi(9, function(err, data){
+	console.log(9, err, data)
 })
 
-async_like(1, function(data){
-	console.log(1, data)
+async_like_multi(8, function(err, data){
+	console.log(8, err, data)
 })
-
-async_like(2, function(data){
-	console.log(2, data)
-})
-
-async_like(3, function(data){
-	console.log(3, data)
-})
-
-async_like(1, function(data){
-	console.log(1, data)
-})
-
-async_like(3, function(data){
-	console.log(3, data)
-})
-
-async_like(10, function(data){
-	console.log(10, data)
-})
-
-async_like(1, function(data){
-	console.log(1, data)
-})
-
-async_like(2, function(data){
-	console.log(2, data)
-})
-
-async_like(3, function(data){
-	console.log(3, data)
-})
-
-async_like(1, function(data){
-	console.log(1, data)
-})
-
-async_like(3, function(data){
-	console.log(3, data)
-})
-
-async_like(10, function(data){
-	console.log(10, data)
-})
-
-async_like(1, function(data){
-	console.log(1, data)
-})
-
-async_like(2, function(data){
-	console.log(2, data)
-})
-
-async_like(3, function(data){
-	console.log(3, data)
-})
-
-async_like(1, function(data){
-	console.log(1, data)
-})
-
-async_like(3, function(data){
-	console.log(3, data)
-})
+//console.log(process.pid, process.memoryUsage())
 
 console.log('request finished')
 
