1c7d7975505d287e5e43f992902d3cb8b82426bf
yhc-yhc <yhc-yhc@users.noreply.github.com>
2016-11-16 14:20:52 +0800

    Update parent2child.js

--- parent2child.js
+++ parent2child.js
@@ -4,25 +4,43 @@ var ev = new events.EventEmitter()
 ev.setMaxListeners(0);
 
 var cp = require('child_process');
-var childs = [];
+var childs = [], flag_obj = {}; 
 
 var process_num = process.env.process_num || 3;
 for(var i=0; i<process_num; i++){
 	var child = cp.fork('./child.js');
-		child.on('message', function(m){
+	child.on('message', function(m){
 		//console.log(m)
+		var pid = m.pid;
+		var respose_child = childs.filter(function(e, i, a){
+			return e.pid == pid;
+		})[0];
+		respose_child.busy--;
+		delete flag_obj[m.flag];
 		ev.emit(m.flag, m.error, m.data)
 	})
+	child.busy = 0;
 	childs.push(child);
+}
+
+function getMostFreeChild(){
+	return childs.sort(function(c, p){
+		return c.busy - p.busy
+	})[0];
 }
 
 module.exports = function(opt, fn){
 	var _flag = opt.flag;
 	var data = opt.data;
 	var flag = _flag + '___' + JSON.stringify(data)
-	var child = childs.shift();
-	child.send({flag: flag, data: data});
-	childs.push(child)
+
+	if(!flag_obj[flag]){
+		flag_obj[flag] = 1;
+		var child = getMostFreeChild();
+		child.busy ++ ;
+		child.send({flag: flag, data: data});
+	}
+	
 	ev.once(flag, function(err, data){
 		fn(err, data)
 	})
